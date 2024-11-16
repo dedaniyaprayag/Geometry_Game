3e83a0b810616567c71a0fafe25fc5294f551fcf
dedaniyaprayag <dedaniyaprayag@gmail.com>
2024-11-16 15:50:31 -0500

    Added Geometry Game

---
+++ main.py
@@ -0,0 +1,20 @@
+import turtle
+
+# Create a canvas instance
+myturtle = turtle.Turtle()
+
+# goto a certain certain canvas
+myturtle.penup()
+myturtle.goto(50,75)
+
+myturtle.pendown()
+myturtle.forward(100)
+myturtle.left(90)
+myturtle.forward(200)
+myturtle.left(90)
+myturtle.forward(100)
+myturtle.left(90)
+myturtle.forward(200)
+
+
+turtle.done()
