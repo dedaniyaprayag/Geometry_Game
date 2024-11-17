d97fbbd7387c7450e8e1e4a971ca28a1117d9f21
dedaniyaprayag <dedaniyaprayag@gmail.com>
2024-11-17 14:41:39 -0500

    Added Child Glass to draw rectangle and point using canvas

--- geometry_game_v1.py
+++ geometry_game_v1.py
@@ -1,4 +1,5 @@
 from random import randint
+import turtle
 
 class Point:
     
@@ -24,8 +25,45 @@ class Rectangle:
             (self.point2.y - self.point1.y)
     
 
+class GraphicalRectangle(Rectangle):
+
+    def draw(self,canvas):
+        # goto a certain certain canvas
+        canvas.penup()
+        canvas.goto(self.point1.x,self.point1.y)
+
+        canvas.pendown()
+        canvas.forward(self.point2.x - self.point1.x)
+        canvas.left(90)
+        canvas.forward(self.point2.y - self.point1.y)
+        canvas.left(90)
+        canvas.forward(self.point2.x - self.point1.x)
+        canvas.left(90)
+        canvas.forward(self.point2.y - self.point1.y)
+        
+       # turtle.done()
+
+
+# gui_rectangle = GraphicalRectangle(Point(randint(0,400),randint(0,400)),
+#                       Point(randint(10,400),randint(10,400)))
+
+# myturle = turtle.Turtle()
+
+# gui_rectangle.draw(canvas=myturle)
+
+# print(gui_rectangle.area())
+
+class GuiPoint(Point):
+    
+    def draw(self,canvas,size=5,color='red'):
+        canvas.penup()
+        canvas.goto(self.x,self.y)
+        canvas.pendown()
+        canvas.dot(size, color)
+
+
 # create rectangle object
-rectangle = Rectangle(Point(randint(0,400),randint(0,400)),
+rectangle = GraphicalRectangle(Point(randint(0,400),randint(0,400)),
                       Point(randint(10,400),randint(10,400)))
 print("Rectangle Coordinates:",
       rectangle.point1.x,",",
@@ -33,12 +71,16 @@ print("Rectangle Coordinates:",
       rectangle.point2.x,",",
       rectangle.point2.y)
 
-user_point = Point(float(input("Guess x: ")),float(input("Guess y:")))
+user_point = GuiPoint(float(input("Guess x: ")),float(input("Guess y:")))
 user_area = float(input("Guess rectangle area: "))
 
 print("Your point was inside rectangle: ",user_point.falls_in_rectangle(rectangle))
 print("Your area was off by: ",rectangle.area() - user_area)
 
+myturle1 = turtle.Turtle()
+rectangle.draw(canvas=myturle1)
+user_point.draw(canvas=myturle1)
+turtle.done()
     
 
         
